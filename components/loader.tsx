eb1247299d077da3a6d527a83ac1eeb2a70d5ee7
Gerald Gonzales <94728635+ggmnjjbkkv@users.noreply.github.com>
2024-02-23 10:43:01 +0800

    commit

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,17 +1,11 @@
-import Image from "next/image"
+import { FaSpinner } from "react-icons/fa";
 
 export const Loader = () => {
   return (
     <div className="h-full flex flex-col gap-y-4 items-center justify-center">
-      <div className="w-10 h-10 relative animate-spin">
-        <Image
-          alt="Logo"
-          src="/logo.png"
-          fill
-        />
-      </div>
+      <FaSpinner className="animate-spin"/>
       <p className="text-sm text-muted-foreground">
-        Genius is thinking...
+        Generating...
       </p>
     </div>
   );
